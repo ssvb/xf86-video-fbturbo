ssvb/xf86-video-fbturbo

commit 9e0a87319b90e3e364fde7cffd24662926f5a4fa

DRI2: Workaround window resize bug in Mali r3p0 blob
The Mali blob is doing something like this:

 1. Request BackLeft DRI2 buffer (buffer A) and render to it
 2. Swap buffers
 3. Request BackLeft DRI2 buffer (buffer B)
 4. Check window size, and if it has changed - go back to step 1.
 5. Render to the current back buffer (either buffer A or B)
 6. Swap buffers
 7. Go back to step 4

A very serious show stopper problem is that the Mali blob ignores
DRI2-InvalidateBuffers events and just uses GetGeometry polling
to check whether the window size has changed. Unfortunately this
is racy and we may end up with a size mismatch between buffer A
and buffer B. This is particularly easy to trigger when the window
size changes exactly between steps 1 and 3.

See test/gles-yellow-blue-flip.c program which demonstrates this.
Qt5 applications also trigger this bug.

We workaround the issue by explicitly tracking the requests for
BackLeft buffers and checking whether the sizes of these buffers
match at step 1 and step 3. However the real challenge here is
notifying the client application that these buffers are no good,
so that it can request them again. As DRI2-InvalidateBuffers
events are ignored, we are in a pretty difficult situation.
Fortunately I remembered a weird behaviour observed earlier:

    https://groups.google.com/forum/#!msg/linux-sunxi/qnxpVaqp1Ys/aVTq09DVih0J

Actually if we return UMP secure ID value 1 for the second DRI2
buffer request, the blob responds to this by spitting out the
following error message:

    [EGL-X11] [2274] DETECTED ONLY ONE FRAMEBUFFER - FORCING A RESIZE
    [EGL-X11] [2274] DRI2 UMP ID 0x3 retrieved
    [EGL-X11] [2274] DRI2 WINDOW UMP SECURE ID CHANGED (0x3 -> 0x3)

And then it proceeds by re-trying to request a pair of DRI2 buffers.
But that's exactly the behaviour we want! As a down side, some ugly
flashing can be seen on screen at the time when this workaround kicks
in, but then everything normalizes. And unfortunately, the race

src/sunxi_mali_ump_dri2.c
@@ -203,6 +203,7 @@ static DRI2Buffer2Ptr MaliDRI2CreateBuffer(DrawablePtr  pDraw,
     sunxi_disp_t            *disp = SUNXI_DISP(pScrn);
     Bool                     can_use_overlay = TRUE;
     PixmapPtr                pWindowPixmap;
+    DRI2WindowStatePtr       window_state = NULL;
 
     if (!(buffer = calloc(1, sizeof *buffer))) {
         ErrorF("MaliDRI2CreateBuffer: calloc failed\n");
@@ -267,7 +268,7 @@ static DRI2Buffer2Ptr MaliDRI2CreateBuffer(DrawablePtr  pDraw,
     /* The default common values */
     buffer->attachment    = attachment;
     buffer->driverPrivate = privates;
-    buffer->format        = format;
+    buffer->format        = format + 1; /* hack to suppress DRI2 buffers reuse */
     buffer->flags         = 0;
     buffer->cpp           = pDraw->bitsPerPixel / 8;
     /* Stride must be 8 bytes aligned for Mali400 */
@@ -282,6 +283,38 @@ static DRI2Buffer2Ptr MaliDRI2CreateBuffer(DrawablePtr  pDraw,
         DebugMsg("Not enough space in the offscreen framebuffer (wanted %d for DRI2)\n",
                  privates->size);
         can_use_overlay = FALSE;
+    }
+
+    /* Allocate the DRI2-related window bookkeeping information */
+    if (attachment == DRI2BufferBackLeft && pDraw->type == DRAWABLE_WINDOW) {
+        HASH_FIND_PTR(private->HashWindowState, &pDraw, window_state);
+        if (!window_state) {
+            window_state = calloc(1, sizeof(*window_state));
+            window_state->pDraw = pDraw;
+            HASH_ADD_PTR(private->HashWindowState, pDraw, window_state);
+            DebugMsg("Allocate DRI2 bookkeeping for window %p\n", pDraw);
+        }
+        window_state->buf_request_cnt++;
+    }
+
+    /* For odd buffer requests save the window size */
+    if (window_state && (window_state->buf_request_cnt & 1)) {
+        /* remember window size for one buffer */
+        window_state->width = pDraw->width;
+        window_state->height = pDraw->height;
+    }
+
+    /* For even buffer requests check if the window size is still the same */
+    if (window_state && !(window_state->buf_request_cnt & 1) &&
+                         (pDraw->width != window_state->width ||
+                          pDraw->height != window_state->height)) {
+        DebugMsg("DRI2 buffers size mismatch detected, trying to recover\n");
+        privates->handle = UMP_INVALID_MEMORY_HANDLE;
+        privates->addr   = NULL;
+        buffer->name  = 1;
+        buffer->cpp   = 0;
+        buffer->pitch = 0;
+        return buffer;
     }
 
     if (can_use_overlay) {
@@ -433,6 +466,9 @@ static void MaliDRI2CopyRegion(DrawablePtr   pDraw,
     MaliDRI2BufferPrivatePtr bufpriv = (MaliDRI2BufferPrivatePtr)pSrcBuffer->driverPrivate;
     sunxi_disp_t *disp = SUNXI_DISP(xf86Screens[pScreen->myNum]);
 
+    if (!bufpriv->addr)
+        return;
+
     UpdateOverlay(pScreen);
 
     if (!drvpriv->bOverlayWinEnabled || bufpriv->handle != UMP_INVALID_MEMORY_HANDLE) {
@@ -530,6 +566,14 @@ DestroyWindow(WindowPtr pWin)
     ScrnInfoPtr pScrn = xf86Screens[pScreen->myNum];
     SunxiMaliDRI2 *private = SUNXI_MALI_UMP_DRI2(pScrn);
     Bool ret;
+    DrawablePtr pDraw = &pWin->drawable;
+    DRI2WindowStatePtr window_state = NULL;
+    HASH_FIND_PTR(private->HashWindowState, &pDraw, window_state);
+    if (window_state) {
+        DebugMsg("Free DRI2 bookkeeping for window %p\n", pWin);
+        HASH_DEL(private->HashWindowState, window_state);
+        free(window_state);
+    }
 
     if (pWin == private->pOverlayWin) {
         sunxi_disp_t *disp = SUNXI_DISP(pScrn);

src/sunxi_mali_ump_dri2.h
@@ -47,6 +47,38 @@ typedef struct
     size_t                  height;
 } UMPBufferInfoRec, *UMPBufferInfoPtr;
 
+/*
+ * DRI2 related bookkeeping for windows. Because Mali r3p0 blob has
+ * quirks and needs workarounds, we can't fully rely on the Xorg DRI2
+ * framework. But instead have to predict what is happening on the
+ * client side based on the typical blob behavior.
+ *
+ * The blob is doing something like this:
+ *  1. Requests BackLeft DRI2 buffer (buffer A) and renders to it
+ *  2. Swaps buffers
+ *  3. Requests BackLeft DRI2 buffer (buffer B)
+ *  4. Checks window geometry, and if it has changed - go back to step 1.
+ *  5. Renders to the current back buffer (either buffer A or B)
+ *  6. Swaps buffers
+ *  7. Go back to step 4
+ *
+ * The main problem is that The Mali blob ignores DRI2-InvalidateBuffers
+ * events and just uses GetGeometry polling to check whether the window
+ * size has changed. Unfortunately this is racy and we may end up with a
+ * size mismatch between buffer A and buffer B. This is particularly easy
+ * to trigger when the window size changes exactly between steps 1 and 3.
+ * See test/gles-yellow-blue-flip.c program which demonstrates this.
+ */
+typedef struct
+{
+    UT_hash_handle          hh;
+    DrawablePtr             pDraw;
+    /* width and height must be the same for back and front buffers */
+    int                     width, height;
+    /* the number of back buffer requests */
+    int                     buf_request_cnt;
+} DRI2WindowStateRec, *DRI2WindowStatePtr;
+
 typedef struct {
     int                     overlay_x;
     int                     overlay_y;
@@ -68,6 +100,7 @@ typedef struct {
     ump_secure_id           ump_fb_secure_id;
 
     UMPBufferInfoPtr        HashPixmapToUMP;
+    DRI2WindowStatePtr      HashWindowState;
 
     int                     drm_fd;
 } SunxiMaliDRI2;

commit 0fd7d5de06cf889cd34a4ee7c086d77950f84332

DRI2: CPU copy fallback path does not drop half of the frames anymore
The recent commit 9e0a87319b90e3e364fde7cffd24662926f5a4fa (its part
that suppressed buffers reuse in the Xorg DRI2 framework) introduced
a regression. Half of the frames stoppped reaching the screen on
the CPU copy fallback path because the Mali blob now ended up
rendering them to the "wrong" buffer.

It just confirms that we need to completely move from the standard
DRI2 framework in the Xorg server to our own buffers bookkeeping
logic. This patch fixes the regression by introducing a single UMP
buffer per window, which is shared between back and front DRI2
buffers. We can do this because double buffering does not make much
sense on the fallback path at the moment (we can't set scanout from
this buffer and anyway have to copy this data elsewhere immediately
after we get it from Mali).

Signed-off-by: Siarhei Siamashka <siarhei.siamashka@gmail.com>

src/sunxi_mali_ump_dri2.c
@@ -190,6 +190,23 @@ static void UpdateOverlay(ScreenPtr pScreen);
 typedef UMPBufferInfoRec MaliDRI2BufferPrivateRec;
 typedef UMPBufferInfoPtr MaliDRI2BufferPrivatePtr;
 
+static void unref_ump_buffer_info(UMPBufferInfoPtr umpbuf)
+{
+    if (--umpbuf->refcount <= 0) {
+        DebugMsg("unref_ump_buffer_info(%p) [refcount=%d, handle=%p]\n",
+                 umpbuf, umpbuf->refcount, umpbuf->handle);
+        if (umpbuf->handle != UMP_INVALID_MEMORY_HANDLE) {
+            ump_mapped_pointer_release(umpbuf->handle);
+            ump_reference_release(umpbuf->handle);
+        }
+        free(umpbuf);
+    }
+    else {
+        DebugMsg("Reduced ump_buffer_info %p refcount to %d\n",
+                 umpbuf, umpbuf->refcount);
+    }
+}
+
 static DRI2Buffer2Ptr MaliDRI2CreateBuffer(DrawablePtr  pDraw,
                                            unsigned int attachment,
                                            unsigned int format)
@@ -336,6 +353,25 @@ static DRI2Buffer2Ptr MaliDRI2CreateBuffer(DrawablePtr  pDraw,
         }
     }
     else {
+        /* Reuse the existing UMP buffer if we can */
+        if (window_state->ump_mem_buffer_ptr &&
+            window_state->ump_mem_buffer_ptr->size == privates->size &&
+            window_state->ump_mem_buffer_ptr->depth == privates->depth &&
+            window_state->ump_mem_buffer_ptr->width == privates->width &&
+            window_state->ump_mem_buffer_ptr->height == privates->height) {
+
+            free(privates);
+
+            privates = window_state->ump_mem_buffer_ptr;
+            privates->refcount++;
+            buffer->driverPrivate = privates;
+            buffer->name = ump_secure_id_get(privates->handle);
+
+            DebugMsg("Reuse the already allocated UMP buffer %p, ump=%d\n",
+                     privates, buffer->name);
+            return buffer;
+        }
+
         /* Allocate UMP memory buffer */
 #ifdef HAVE_LIBUMP_CACHE_CONTROL
         privates->handle = ump_ref_drv_allocate(privates->size,
@@ -356,6 +392,13 @@ static DRI2Buffer2Ptr MaliDRI2CreateBuffer(DrawablePtr  pDraw,
         privates->addr = ump_mapped_pointer_get(privates->handle);
         buffer->name = ump_secure_id_get(privates->handle);
         buffer->flags = 0;
+
+        /* Replace the old UMP buffer with the newly allocated one */
+        if (window_state->ump_mem_buffer_ptr)
+            unref_ump_buffer_info(window_state->ump_mem_buffer_ptr);
+
+        window_state->ump_mem_buffer_ptr = privates;
+        privates->refcount++;
     }
 
     DebugMsg("DRI2CreateBuffer win=%p, buf=%p:%p, att=%d, ump=%d:%d, w=%d, h=%d, cpp=%d, depth=%d\n",
@@ -381,17 +424,7 @@ static void MaliDRI2DestroyBuffer(DrawablePtr pDraw, DRI2Buffer2Ptr buffer)
 
     if (buffer != NULL) {
         privates = (MaliDRI2BufferPrivatePtr)buffer->driverPrivate;
-        if (!privates->pPixmap) {
-            /* If pPixmap != 0, then these are freed in DestroyPixmap */
-            if (privates->handle != UMP_INVALID_MEMORY_HANDLE) {
-                ump_mapped_pointer_release(privates->handle);
-                ump_reference_release(privates->handle);
-            }
-        }
-        if (--privates->refcount <= 0) {
-            DebugMsg("free(privates)\n");
-            free(privates);
-        }
+        unref_ump_buffer_info(privates);
         free(buffer);
     }
 }
@@ -575,6 +608,8 @@ DestroyWindow(WindowPtr pWin)
     if (window_state) {
         DebugMsg("Free DRI2 bookkeeping for window %p\n", pWin);
         HASH_DEL(private->HashWindowState, window_state);
+        if (window_state->ump_mem_buffer_ptr)
+            unref_ump_buffer_info(window_state->ump_mem_buffer_ptr);
         free(window_state);
     }
 
@@ -650,15 +685,9 @@ DestroyPixmap(PixmapPtr pPixmap)
         pPixmap->devKind = umpbuf->BackupDevKind;
         pPixmap->devPrivate.ptr = umpbuf->BackupDevPrivatePtr;
 
-        ump_mapped_pointer_release(umpbuf->handle);
-        ump_reference_release(umpbuf->handle);
-
         HASH_DEL(self->HashPixmapToUMP, umpbuf);
-        DebugMsg("umpbuf->refcount=%d\n", umpbuf->refcount);
-        if (--umpbuf->refcount <= 0) {
-            DebugMsg("free(umpbuf)\n");
-            free(umpbuf);
-        }
+        umpbuf->pPixmap = NULL;
+        unref_ump_buffer_info(umpbuf);
     }
 
     pScreen->DestroyPixmap = self->DestroyPixmap;

src/sunxi_mali_ump_dri2.h
@@ -77,6 +77,9 @@ typedef struct
     int                     width, height;
     /* the number of back buffer requests */
     int                     buf_request_cnt;
+
+    /* allocated UMP buffer (shared between back and front DRI2 buffers) */
+    UMPBufferInfoPtr        ump_mem_buffer_ptr;
 } DRI2WindowStateRec, *DRI2WindowStatePtr;
 
 typedef struct {